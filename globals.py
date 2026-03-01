fc2949d28f7292ec87c049b4dc330bff090cf5f4
noobaritra101-prog <noobaritra.101@gmail.com>
2026-03-02 02:50:58 +0530

    Delete globals.py

--- globals.py
+++
@@ -1,9 +0,0 @@
-# Stores active Telethon clients: {user_id: client_object}
-user_clients = {}
-
-# Stores user settings & stats: {user_id: { 'list': [], 'stats': {}, ... }}
-user_configs = {}
-
-# Stores temporary data for the Telegram OTP web login process
-# Format: { phone_number: { 'client': TelegramClient, 'hash': str } }
-otp_flows = {}
